d90431719db639acbfa11d3b201f7d7f5f3aab64
CindoddCindy <cindodcindy@gmail.com>
2021-05-06 22:31:10 +0700

    tambah package

---
+++ springfcmtest/src/main/java/com/sprinfcm/springfcmtest/SpringfcmtestApplication.java
@@ -0,0 +1,13 @@
+package com.sprinfcm.springfcmtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringfcmtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringfcmtestApplication.class, args);
+	}
+
+}
---
+++ springfcmtest/src/test/java/com/sprinfcm/springfcmtest/SpringfcmtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprinfcm.springfcmtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringfcmtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
